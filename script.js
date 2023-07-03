a8ba9b201a5b11317f51c1c86b8d989b913ecba2
Agostinho Dalte Baptista Soberano <agostinhosoberano2@gmail.com>
2023-07-03 15:47:40 +0200

    adding update button

--- script.js
+++ script.js
@@ -255,9 +255,17 @@ function getCurrentUserComment() {
           </button>
         </div>
       </div>
-      <p class="user-text-comment">
+      <p class="user-text-comment" >
        ${comment.content}
       </p>
+      <div class="fill-button">
+              <button
+                data-send-desktop-btn="sendButton"
+                class="button-common common-button-display"
+              >
+                Update
+              </button>
+            </div>
     </div>
   </div>
   ${getReplies(comment.id) + getCurrentUserReplies(comment.id)}
@@ -379,6 +387,14 @@ function getCurrentUserReplies(id) {
             <p class="user-text-comment">
               ${reply.content}
             </p>
+            <div class="fill-button">
+              <button
+                data-send-desktop-btn="sendButton"
+                class="button-common common-button-display"
+              >
+                Update
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -432,25 +448,3 @@ function render() {
 }
 
 render();
-
-//! just for testing
-// function pushReply() {
-//   dataDB.comments[0].replies.push({
-//     id: 3,
-//     content:
-//       "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-//     createdAt: "1 week ago",
-//     score: 4,
-//     replyingTo: "maxblagun",
-//     user: {
-//       image: {
-//         png: "./images/avatars/image-ramsesmiron.png",
-//         webp: "./images/avatars/image-ramsesmiron.webp",
-//       },
-//       username: "ramsesmiron",
-//     },
-//   });
-//   render();
-// }
-
-// pushReply();
